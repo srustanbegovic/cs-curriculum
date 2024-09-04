3305303f82b5bed04d906d4d3180d0fe46c483bd
srustanbegovic <srustanbegovic8376@student.pps.net>
2024-09-04 15:17:06 -0700

    added horizontal movement to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,27 +5,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    float xspeed;
+    float xdirection;
+    float xvector;
+    float yspeed;
+    float ydirection;
+    float yvector;
+
     public bool overworld; 
 
     private void Start()
     {
         GetComponentInChildren<TopDown_AnimatorController>().enabled = overworld;
         GetComponentInChildren<Platformer_AnimatorController>().enabled = !overworld; //what do you think ! means?
+        xspeed = 4;
+        xdirection = 0;
+        xvector = 0;
         
+        ydirection = 0;
+        yvector = 0;
+
         
         if (overworld)
         {
             GetComponent<Rigidbody2D>().gravityScale = 0f;
+            yspeed = 4;
         }
         else
         {
             GetComponent<Rigidbody2D>().gravityScale = 1;
+            yspeed = 0;
         }
     }
 
     private void Update()
     {
+        xdirection = Input.GetAxis("Horizontal");
+        xvector = xspeed * xdirection * Time.deltaTime;
+        ydirection = Input.GetAxis("Vertical");
+        yvector = yspeed * ydirection * Time.deltaTime;
+
         
+        transform.Translate(xvector, yvector, 0);
+
+
     }
     
     //for organization, put other built-in Unity functions here
