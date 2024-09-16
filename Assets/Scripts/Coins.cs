379d53c41e8f4a0c79a449e99e2dd10f70335633
srustanbegovic <srustanbegovic8376@student.pps.net>
2024-09-16 15:29:55 -0700

    Pick up Coins and added GameManager Script

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    GameManager gm;
+
+    void Start()
+    {
+        gm = FindObjectOfType<GameManager>();   
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            gm.coins += 1;
+            print(gm.coins);
+            Destroy(other.gameObject);
+
+        }
+    }
+
+
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+    public int coins;
+
+
+    void Awake()
+    {
+
+        if (gm != null && gm != this)
+          {
+            Destroy(gameObject);
+
+          }
+        else
+          {
+            gm = this;
+
+        }
+
+        coins = 0;
+
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
