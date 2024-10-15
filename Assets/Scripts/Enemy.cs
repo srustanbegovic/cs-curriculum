ac1d957d4577a14b3f5aedf5cb319b13f770035b
srustanbegovic <srustanbegovic8376@student.pps.net>
2024-10-15 15:30:15 -0700

    moves between partrol points
    doesnt move back to the first one, bounces between second and last

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,15 +10,19 @@ public class Enemy : MonoBehaviour
     private Vector3 direction;
     GameManager gm;
 
-   
+    private Vector3[] points = new Vector3[]
+    {
+        new Vector3(0, -8, 0),
+        new Vector3(-4, -1, 0),
+        new Vector3(-3, -2, 0)
+    };
+
+    private int currentTarget = 0;
     void Start()
     {
-        Vector3[] points = new Vector3[3];
-        points[0] = new Vector3(-2, 1, 0);
-        points[1] = new Vector3(2, 1, 0);
-        points[2] = new Vector3(0, -2, 0);
+        print("point zero" +points[0]);
         speed = 5;
-        target = points[0];
+        target = points[currentTarget];
         gm = FindFirstObjectByType<GameManager>();
         direction = (target - transform.position).normalized; 
 
@@ -27,6 +31,27 @@ public class Enemy : MonoBehaviour
   
     void Update()
     {
+        print(target);
         transform.position += direction * speed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, target) < 0.1f)
+        {
+            print("hit target");
+            ChangeDirection();
+        }
+    }
+
+
+
+
+
+    void ChangeDirection()
+    {
+        currentTarget = (currentTarget + 1) % points.Length;
+        if (currentTarget = 3)
+        {
+            currentTarget = 0;
+        }
+        target = points[currentTarget];
+        direction = (target - transform.position).normalized;
     }
 }
