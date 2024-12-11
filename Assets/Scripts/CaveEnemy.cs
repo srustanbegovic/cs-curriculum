3e2227ab91eda6d4a3f21ed831ab6f42ab0868b2
srustanbegovic <srustanbegovic8376@student.pps.net>
2024-12-11 15:29:11 -0800

    enemy moves but cant die

--- Assets/Scripts/CaveEnemy.cs
+++ Assets/Scripts/CaveEnemy.cs
@@ -1,19 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CaveEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 3f;
+    private Vector3 target;
+    private Vector3 targetdest;
+    private Vector3 direction;
+    public TopDown_AnimatorController panimator;
+    public Vector3[] points = new Vector3[]
+    
+    {
+        new Vector3(6, 0.5f, 0),
+        new Vector3(12, 0.5f, 0),
+    };
+    private int currentTarget = 0;
     void Start()
     {
-    private float speed = 3;
-    public Vector3
+        targetdest = points[currentTarget];
+        direction = (targetdest - transform.position).normalized;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        transform.position += direction * speed * Time.deltaTime;
+        if (transform.position.x < 20 || transform.position.x > 25)
+        {
+            ChangeDirection();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (panimator.IsAttacking)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void ChangeDirection()
+    {
+        currentTarget = (currentTarget + 1) % points.Length;
+        if (currentTarget == 2)
+        {
+            currentTarget = 0;
+        }
+        targetdest = points[currentTarget];
+        direction = (targetdest - transform.position).normalized;
+        //print(targetdest);
     }
 }
